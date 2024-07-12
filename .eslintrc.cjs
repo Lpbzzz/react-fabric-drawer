d9acfe02e046134b81ab04e8216a48c32c27b2b9
liupengbo <liupengbo@vv.cn>
2024-07-12 18:43:58 +0800

    feat: env init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended', // Make sure this is last
+	],
+	plugins: [
+		'react',
+		'react-hooks',
+		'@typescript-eslint',
+		'unused-imports',
+		'prettier',
+	],
+	rules: {
+		'prettier/prettier': 'error',
+		'unused-imports/no-unused-imports': 'error',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
+		],
+		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+		'@typescript-eslint/indent': ['error', 'tab'],
+		'react/react-in-jsx-scope': 'off', // For React 17+ where importing React is not necessary
+		'@typescript-eslint/no-explicit-any': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,47 @@
-const App = ()=> {
-  return (
-    <>
-      app
-    </>
-  )
-}
+import usePreventClickWhenDrag from './hooks/usePreventClickWhenDrag';
+import React from 'react';
 
-export default App
+const Component = () => {
+  const handleClickA = (params: any, ev: React.MouseEvent) => {
+    console.log('___', 'ev', ev.target, ev.currentTarget);
+    if (ev.target !== ev.currentTarget) {
+      console.log('Clicked A', params);
+    }
+  };
+
+  const handleClickB = (ev: React.MouseEvent) => {
+    ev.stopPropagation(); // Prevent event from bubbling up
+    ev.preventDefault();
+    ev.cancelable = false;
+    console.log('Clicked B');
+  };
+
+  const { handleMouseDown, handleMouseMove, handleMouseUp } =
+    usePreventClickWhenDrag({
+      onClick: handleClickA,
+      triggerDistance: 5,
+    });
+
+  return (
+    <div
+      className="a"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={(ev) => handleMouseUp({}, ev)}
+      style={{ border: '1px solid #ccc', padding: 10 }}
+    >
+      <div
+        className="b"
+        onClick={handleClickB}
+        style={{ border: '1px solid #bbb', padding: 10 }}
+      >
+        bbb
+      </div>
+      <div className="c" style={{ border: '1px solid #aaa', padding: 10 }}>
+        cccccc
+      </div>
+    </div>
+  );
+};
+
+export default Component;
